2b766d46ba0022c9015af7d8a67e14b1d1280ab2
i222478 <i222478@nu.edu.pk>
2025-10-01 17:26:59 +0500

    a

--- detect.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-REM Code Smell Detector Shortcut Script
-set "SCRIPT_DIR=%~dp0"
-cd /d "%SCRIPT_DIR%src"
-C:/Users/hashi/AppData/Local/Programs/Python/Python313/python.exe detector_cli.py %*
--- run_gui.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-REM Code Smell Detector GUI Launcher
-echo Starting Code Smell Detector GUI...
-set "SCRIPT_DIR=%~dp0"
-cd /d "%SCRIPT_DIR%src"
-C:/Users/hashi/AppData/Local/Programs/Python/Python313/python.exe detector_gui.py
-pause
